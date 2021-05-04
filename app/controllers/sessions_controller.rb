3ff41fb7a0725b58cb425c69538d9c19120fefb2
ernsykamethelus <69830819+ernsykamethelus@users.noreply.github.com>
2021-05-04 16:34:00 -0400

    updates

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,10 +5,10 @@ class SessionsController < ApplicationController
     end
 
     post '/login' do 
-        @user = User.find_by(:username => params[:username])
-        if @user && @user.authenticate(params[:password])
-            session[:id] = @user.id 
-            redirect :"/users/#{session[:id]}"
+        @current_user = User.find_by(:username => params[:username])
+        if @current_user && @current_user.authenticate(params[:password])
+            session[:id] = @current_user.id 
+            erb :"/users/show"
         else 
             redirect :'/login'
         end
