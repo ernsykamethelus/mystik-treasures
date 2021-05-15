898163ef5ed0c7f4b9c33ae8e0e6bf0e5c7c8977
ernsykamethelus <69830819+ernsykamethelus@users.noreply.github.com>
2021-05-14 20:36:41 -0400

    editing files

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,10 +6,13 @@ class SessionsController < ApplicationController
 
     post "/login" do
         @user = User.find_by(:username => params[:username])
-      
         if @user && @user.authenticate(params[:password])
-          session[:user_id] = user.id
-          erb :"/users/#{session[:id]}"
+            # binding.pry
+          session[:user_id] = @user.id
+          puts "Hello, #{session[:user_id]}"
+        #   erb :"/users/welcome"
+        # erb :"/users/#{session[:id]}" 
+          erb :"/users/welcome"
         else
           redirect "/error"
         end
